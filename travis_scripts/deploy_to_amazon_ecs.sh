d58b33c0f33c52bb68ab9664b3912ddc0e5c8305
jcabelloc <jcabelloc@gmail.com>
2019-09-29 16:49:36 -0500

    Agregar travis al cap 10 - fix - 4

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,5 +1,8 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region us-west-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster sms-fintech-ecs
+# ecs-cli configure --region us-west-2 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster sms-fintech-ecs
+ecs-cli configure profile --profile-name sms-cap-10 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY 
+ecs-cli configure --cluster sms-fintech-ecs --default-launch-type EC2 --region 	us-west-2 --config-name config-sms-fintech-ecs
+
 ecs-cli compose --file docker/docker-compose-travis.yml up
 rm -rf ~/.ecs
 
